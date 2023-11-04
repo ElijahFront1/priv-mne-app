e393cc754444315901c5340e5b0ac7bae74fc3a2
Ilia Bazyrev <bazyrev1@gmail.com>
2023-11-04 19:16:23 +0300

    Configure ESLint, Prettier and set up folder structure with FSD with a boilerplate

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,17 +1,129 @@
 module.exports = {
-    root: true,
-    env: { browser: true, es2020: true },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react-hooks/recommended',
-        'prettier',
-        'plugin:prettier/recommended',
+  env: { browser: true, es2020: true },
+  extends: [
+    'plugin:eslint-plugin-import/recommended',
+    'plugin:react-hooks/recommended',
+    'eslint-config-airbnb',
+    'eslint-config-prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': 'off',
+    'import/prefer-default-export': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'import/order': [
+      'error',
+      {
+        pathGroups: [
+          { pattern: 'react', group: 'builtin' },
+          { pattern: 'vite', group: 'builtin' },
+          { pattern: '~shared/**', group: 'internal' },
+          { pattern: '~entities/**', group: 'internal' },
+          { pattern: '~features/**', group: 'internal' },
+          { pattern: '~widgets/**', group: 'internal' },
+          { pattern: '~pages/**', group: 'internal' },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        'newlines-between': 'never',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
     ],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['react-refresh', '@typescript-eslint'],
-    rules: {
-        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: [
+          {
+            group: [
+              '~shared/*/*/**',
+              '~entities/*/**',
+              '~features/*/**',
+              '~widgets/*/**',
+              '~pages/*/**',
+              '~app/**',
+            ],
+            message:
+              'Direct access to the internal parts of the module is prohibited',
+          },
+          {
+            group: [
+              '../**/shared',
+              '../**/entities',
+              '../**/features',
+              '../**/widgets',
+              '../**/pages',
+              '../**/app',
+            ],
+            message: 'Prefer absolute imports instead of relatives',
+          },
+        ],
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: ['./vite.config.ts'] },
+    ],
+  },
+  overrides: [
+    // typescript
+    {
+      files: ['./src/**/*.ts', './src/**/*.tsx'],
+      extends: [
+        'plugin:eslint-plugin-import/typescript',
+        'eslint-config-airbnb-typescript',
+      ],
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['tsconfig.json'],
+      },
+      plugins: ['@typescript-eslint/eslint-plugin'],
+      rules: {
+        '@typescript-eslint/indent': 'off',
+        'import/no-extraneous-dependencies': [
+          'error',
+          {
+            devDependencies: [
+              '**/msw/**',
+              '**/react-query/utils.tsx',
+              '**/react-router/utils.ts',
+            ],
+          },
+        ],
+      },
     },
+    // tests
+    {
+      files: ['./src/**/*.test.ts', './src/**/*.test.tsx'],
+      extends: ['plugin:testing-library/react', 'plugin:jest-dom/recommended'],
+      rules: {
+        'testing-library/no-debugging-utils': 'warn',
+        'import/no-extraneous-dependencies': [
+          'error',
+          { devDependencies: true },
+        ],
+      },
+    },
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
 };
